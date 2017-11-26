6eb81dae0cde3a613865af7e5100b08e600865d7
EstelleAlisa <1018553346@qq.com>
2017-11-26 16:43:33 +0800

    project

--- lefeng/src/App.js
+++ lefeng/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import LoginCom from './Components/LoginCom';
+import RegisterCom from './Components/RegisterCom';
 import {
   BrowserRouter as Router,
   Route,
@@ -13,8 +14,16 @@ class App extends Component {
     return (
       <div className="App">
           <Router>
-              <LoginCom></LoginCom>                  
-          </Router>
+              <div className='real'>
+                  <main>
+                  <Switch>
+                      <Redirect exact from='/' to='/home'/>
+                      <Route path='/login' component={LoginCom}/>
+                      <Route path='/register'  component={RegisterCom}/>
+                  </Switch>
+                  </main>
+              </div>
+            </Router>
           
       </div>
     );
---
+++ lefeng/src/Components/RegisterCom.js
@@ -0,0 +1,33 @@
+import React, {Component} from 'react';
+import axios from 'axios';
+import {
+  Link
+} from 'react-router-dom'
+import '../Css/Register.css'
+
+class RegisterUI extends Component{
+	componentDidMount() {
+		
+	}
+	render() {
+		var props = this.props;
+		return(
+			<div className='register'>
+				<div className='top'>
+					<Link to='/login'><i class="iconfont">&#xe608;</i></Link>
+					<h2>注册</h2>
+					<span><i class="iconfont"></i></span>
+				</div>
+				<div className='bottom'>
+					 	<input type='text' placeholder='已验证手机/用户名' className='i1 l'/><br/>
+						<button className='l reg'>获取验证码</button><br/>
+						<input type='text' placeholder='请输入短信验证码' className='i2' /><br/>	
+						<input type='password' placeholder='请输入密码' className='i2' /><br/>
+						<button className='regi'>注册</button>
+				</div>
+			</div>
+		)
+	}
+}
+
+export default RegisterUI;
